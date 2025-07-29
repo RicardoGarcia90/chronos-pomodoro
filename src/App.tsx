b8bc1bf3ab147dbb69ece168f4ef2fcf1ee46640
Ricardo Garci <ricardonunesgarcia@outlook.com>
2025-07-29 15:58:23 -0300

    css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-function App() {
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
+
+export function App() {
   return (
-    <div>
-      <h1>Olá Mundo!</h1>
+    <>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, dolores
         sapiente porro similique ipsum, harum officiis accusamus aperiam nihil
         tempora expedita, suscipit impedit ex dignissimos id dolor quisquam
         ipsam. Necessitatibus!
       </p>
-    </div>
+    </>
   );
 }
-
-export default App;
