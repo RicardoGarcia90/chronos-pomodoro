e403abfa5ffabb38de89535f9dd59c9af7b05208
Ricardo Garci <ricardonunesgarcia@outlook.com>
2025-08-05 16:47:26 -0300

    aulas de 18 a 20

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, dolores
-        sapiente porro similique ipsum, harum officiis accusamus aperiam nihil
-        tempora expedita, suscipit impedit ex dignissimos id dolor quisquam
-        ipsam. Necessitatibus!
-      </p>
+      <Container>
+        <section>LOGO</section>
+      </Container>
+
+      <Container>
+        <section>MENU</section>
+      </Container>
     </>
   );
 }
